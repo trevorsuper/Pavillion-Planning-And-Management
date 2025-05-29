0d61480083cb446f512f5e84cd7935af5ed48023
joshuapagonas <65562873+joshuapagonas@users.noreply.github.com>
2025-05-28 21:47:47 -0400

    Restored Backend.

---
+++ api/webapi/PPMDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using PPM.Models;
+
+namespace PPM
+{
+    public class PPMDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+
+        public PPMDBContext() : base() { }
+        public PPMDBContext(DbContextOptions options) : base(options) { }
+    }
+}
