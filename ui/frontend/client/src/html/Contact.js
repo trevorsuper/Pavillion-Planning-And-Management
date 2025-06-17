2035eb34fe6726f96173cf20ba0073257d5907e5
zeleney30 <michaelzeleney@gmail.com>
2025-06-17 13:41:59 -0400

    Add Contact Form
    Added a form to the contact page with relevant input elements such as email, phone number, name, and message.

--- ui/frontend/client/src/html/Contact.js
+++ ui/frontend/client/src/html/Contact.js
@@ -3,7 +3,15 @@ import React, { useEffect, useState } from 'react';
 function Contact() {
 
   return <div>{/*{message}*/}
-    <h1>Contact</h1>
+    <h1>Contact Us</h1>
+    <form>
+        <input type="email" placeholder="Email"></input>
+        <input type="tel" placeholder="(111)-222-3333"></input>
+        <input type="text" placeholder="First Name"></input>
+        <input type="text" placeholder="Last Name"></input>
+        <input type="text" placeholder="Message..."></input>
+        <input type="Submit"></input>
+    </form>
   </div>;
 }
 
