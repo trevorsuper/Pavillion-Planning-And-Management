30d64d5a4b31a1730feb312c1c5bef51478663fd
zeleney30 <michaelzeleney@gmail.com>
2025-06-17 13:44:47 -0400

    Moved the image carousel
    Moved the image carousel to the Home page so its only displayed on the home page instead of every page.
    
    Added Explore page.
    
    Also added the same carousel to the Explore the Parks page.

---
+++ ui/frontend/client/src/html/Explore.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+function Explore() {
+
+  return <div>{/*{message}*/}
+    <h1>Explore the Parks</h1>
+    <img src="" alt="Image carousel of different parks in Troy."></img>
+  </div>;
+}
+
+export default Explore;
--- ui/frontend/client/src/html/Home.js
+++ ui/frontend/client/src/html/Home.js
@@ -4,6 +4,7 @@ function Home() {
 
   return <div>{/*{message}*/}
     <h1>Home</h1>
+    <image src="" alt="Image carousel of different parks in Troy."></image>
   </div>;
 }
 
