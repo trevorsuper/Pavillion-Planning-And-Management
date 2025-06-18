aea938658773b73e589cfd9f80b3decaa4af8dcc
zeleney30 <michaelzeleney@gmail.com>
2025-06-18 14:47:27 -0400

    Add image carousel to Explore page
    Added an image carousel that rotates through images in a list.
    
    Also added normal images below the carousel of each of the parks.

--- ui/frontend/client/src/html/Contact.js
+++ ui/frontend/client/src/html/Contact.js
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react';
 
 function Contact() {
 
-  return <div>{/*{message}*/}
+  return <div>
     <h1>Contact Us</h1>
     <form>
         <input type="email" placeholder="Email"></input>
         <input type="tel" placeholder="(111)-222-3333"></input>
+        <br></br>
         <input type="text" placeholder="First Name"></input>
         <input type="text" placeholder="Last Name"></input>
+        <br></br>
         <input type="text" placeholder="Message..."></input>
         <input type="Submit"></input>
     </form>
