62ef0c35dc509d37ccf7152bb68bb57592861cbe
zeleney30 <michaelzeleney@gmail.com>
2025-06-30 13:49:40 -0400

    Contact form styling
    Added styling to the Contact Us page.

--- ui/frontend/client/src/html/Contact.js
+++ ui/frontend/client/src/html/Contact.js
@@ -1,19 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import '../css/contact.css';
 
 function Contact() {
 
   return <div>
-    <h1>Contact Us</h1>
-    <form>
-        <input type="email" placeholder="Email"></input>
-        <input type="tel" placeholder="(111)-222-3333"></input>
-        <br></br>
-        <input type="text" placeholder="First Name"></input>
-        <input type="text" placeholder="Last Name"></input>
-        <br></br>
-        <input type="text" placeholder="Message..."></input>
-        <input type="Submit"></input>
-    </form>
+    <h1 class="title">Contact Us</h1>
+    <div class="form-container">
+      <form>
+        <div class="form-row">
+          <input type="email" placeholder="Email"></input>
+          <input type="tel" placeholder="(111)-222-3333"></input>
+        </div>
+        
+        <div class="form-row">
+          <input type="text" placeholder="First Name"></input>
+          <input type="text" placeholder="Last Name"></input>
+        </div>
+
+        <textarea placeholder="Message..." class="form-message"></textarea>
+        <input type="Submit" class="form-submit"></input>
+      </form>
+    </div>
   </div>;
 }
 
