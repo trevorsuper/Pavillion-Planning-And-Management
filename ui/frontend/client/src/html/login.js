997eeaaa12aa2a1f488bb3183d761acb98c9e48d
joshuapagonas <65562873+joshuapagonas@users.noreply.github.com>
2025-08-08 00:16:05 -0400

    Fixed Registration Phone Number Issue

--- ui/frontend/client/src/html/login.js
+++ ui/frontend/client/src/html/login.js
@@ -63,6 +63,7 @@ function Login() {
           username: formData.username,
           email: formData.email,
           password: formData.password,
+          phone_number: formData.phoneNumber,
         };
 
     try {
